569e93a54acd4cb1c889cff779b7a8678fdc3754
salimmancer <salim.mancer@se.univ-bejaia.dz>
2026-05-01 23:19:02 +0200

    fix: alias vite @algbnb/core

--- apps/web-view/vite.config.js
+++ apps/web-view/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@algbnb/core': path.resolve(__dirname, '../../packages/core/index.js'),
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
